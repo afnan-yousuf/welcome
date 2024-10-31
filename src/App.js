cd6ca22d5fdca4d2859eb64e89e99c3d1c1db1cb
afnan-yousuf <afnanyousuf@gmail.com>
2024-10-31 17:09:03 +0500

    firstcomment

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+import Profilez from './Profilez';
+
+function App() {
+  
+  return (
+    <>
+      <p>This is paragraph</p>
+      <p>This is paragraph</p>
+      <Profilez name="Afnan Yousuf" detail="Former Naat Reciter" designation="senior"/>
+      <Profilez name="Amin" detail="Former footballer" designation="junior"/>
+    </>
+
+  );
+}
+
+export default App;
